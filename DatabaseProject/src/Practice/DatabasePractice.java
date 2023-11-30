c81284637b3cfbc7188e1bd1fa95d1d2b29922b7
mbashir1 <mbashir1@N6107S41-INST.ad.douglascollege.ca>
2023-11-30 11:38:28 -0800

    This is a second commit

--- DatabaseProject/src/Practice/DatabasePractice.java
+++ DatabaseProject/src/Practice/DatabasePractice.java
@@ -9,7 +9,7 @@ public class DatabasePractice {
 		Connection conn =null;
 		Statement stat =null;
 		ResultSet rs = null;
-		
+		method();
 		try {
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 		}
@@ -67,5 +67,10 @@ public class DatabasePractice {
 			}
 		}
 	}
+	
+	public static void method() {
+		System.out.println("This is a method");
+	}
+	
 
 }
